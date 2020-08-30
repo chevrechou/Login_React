315be93e324e40bd6a4408588bd9a8e409815d2b
michael <michael@admins-MacBook-Pro.local>
2020-08-29 20:53:24 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import './App.css';
 import LoginButton from './Login';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
+
 import Home from './Home';
 function App() {
   const {isLoading} = useAuth0;
+  const { user, isAuthenticated } = useAuth0();
+
   if (isLoading) return <div>Loading...</div>
   
   return (
     <div className="App">
       <LoginButton/>
       <LogoutButton/>
-      <Home/>
+      <Home isAuthenticated={isAuthenticated}/>
       
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
-
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import About from './About';
+import Users from './Users';
 class Home extends Component {
-    call(){
-    
-        fetch("https://genius.p.rapidapi.com/artists/16775/songs", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "genius.p.rapidapi.com",
-		"x-rapidapi-key": "514130934dmsh2daaa7ac7640d19p10cb35jsne75e2504da10"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.log(err);
-});
+    constructor(props){
+        super(props);
+        console.log(this.props)
     }
+    
     render() {
+        const isAuthenticated=this.props.isAuthenticated;
         return (
             <div>
-                
+                {isAuthenticated ?
+                <Router>
+                <div>
+                  <nav>
+                    <ul>
+                      <li>
+                        <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                        <Link to="/about">About</Link>
+                      </li>
+                      <li>
+                        <Link to="/users">Users</Link>
+                      </li>
+                    </ul>
+                  </nav>
+          
+                  {/* A <Switch> looks through its children <Route>s and
+                      renders the first one that matches the current URL. */}
+                  <Switch>
+                    <Route path="/about">
+                      <About />
+                    </Route>
+                    <Route path="/users">
+                      <Users />
+                    </Route>
+                    
+                  </Switch>
+                </div>
+              </Router> :
+                null
+            }
             </div>
+            
         );
     }
 }
